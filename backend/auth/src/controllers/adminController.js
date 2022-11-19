9a854fbc8f426de00314ed2c541ef435294290e0
Marsulami <traidat8sao@gmail.com>
2022-11-19 22:48:55 +0700

    fix bug

--- backend/auth/src/controllers/adminController.js
+++ backend/auth/src/controllers/adminController.js
@@ -1,4 +1,4 @@
-const User = require("../models/userModel");
+const User = require('../models/userModel');
 
 class AdminController {
 	async deleteUser(req, res) {
@@ -6,24 +6,24 @@ class AdminController {
 			const user = await User.findById(req.params.id);
 			if (!user) {
 				return res.status(400).json({
-					message: "Người dùng không tồn tại",
-					status: "Thất bại",
+					message: 'Người dùng không tồn tại',
+					status: 'Thất bại',
 				});
 			}
-			await user.active = false;
+			user.active = false;
+
 			await user.save();
 			return res.status(200).json({
-				status: "Thành công",
-				message: "Xóa người dùng thành công",
+				status: 'Thành công',
+				message: 'Xóa người dùng thành công',
 			});
 		} catch (err) {
 			console.error(err.message);
 			res.status(400).json({
-				message: "Xóa người dùng không thành công",
-				status: "Thất bại",
+				message: 'Xóa người dùng không thành công',
+				status: 'Thất bại',
 			});
 		}
 	}
 }
 module.exports = new AdminController();
-
